2d4536c0a49a4addc1bc1550aa7ef266315c1b81
Brandon Nutter <brandon@metricstory.com>
2021-01-02 18:13:18 -0800

    Added configuration changes to shopify-example

--- server.js
+++ server.js
@@ -1,88 +1,91 @@
-require('isomorphic-fetch');
-const dotenv = require('dotenv');
-const Koa = require('koa');
-const next = require('next');
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const session = require('koa-session');
-const { Pool, Client } = require('pg')
-
+require("isomorphic-fetch");
+const dotenv = require("dotenv");
+const Koa = require("koa");
+const next = require("next");
+const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth");
+const { verifyRequest } = require("@shopify/koa-shopify-auth");
+const session = require("koa-session");
+const { Pool, Client } = require("pg");
 
 dotenv.config();
-const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
-const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
-const getSubscriptionUrl = require('./server/getSubscriptionUrl');
+const { default: graphQLProxy } = require("@shopify/koa-shopify-graphql-proxy");
+const { ApiVersion } = require("@shopify/koa-shopify-graphql-proxy");
+const getSubscriptionUrl = require("./server/getSubscriptionUrl");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 // pools will use environment variables for connection information
-const pool = new Pool({ssl: { rejectUnauthorized: false }})
+const pool = new Pool({ ssl: { rejectUnauthorized: false } });
 
-const text = 'INSERT INTO public.shop(shop, access_token, date_joined, user_scope, user_first_name, user_last_name, user_email, locale, is_owner, is_collaborator, is_email_verified, is_active) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *'
+const sqlInsertUser =
+  "INSERT INTO public.shop(shop, access_token, date_joined, user_scope, user_first_name, user_last_name, user_email, locale, is_owner, is_collaborator, is_email_verified, is_active) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *";
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 app.prepare().then(() => {
-    const server = new Koa();
-    server.use(session({ secure: true, sameSite: 'none' }, server));
-    server.keys = [SHOPIFY_API_SECRET_KEY];
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: "none" }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
 
-    server.use(
-        createShopifyAuth({
-            apiKey: SHOPIFY_API_KEY,
-            secret: SHOPIFY_API_SECRET_KEY,
-            scopes: ['read_products', 'write_products'],
-            async afterAuth(ctx) {
-                const { shop, accessToken, associatedUserScope, associatedUser } = ctx.session;
-                ctx.cookies.set('shopOrigin', shop, {
-                    httpOnly: false,
-                    secure: true,
-                    sameSite: 'none'
-                });
-                await getSubscriptionUrl(ctx, accessToken, shop);
+  server.use(
+    // Gets called on installation & when a customer does not have a session
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ["read_products", "write_products"],
+      async afterAuth(ctx) {
+        const {
+          shop,
+          accessToken,
+          associatedUserScope,
+          associatedUser,
+        } = ctx.session;
+        ctx.cookies.set("shopOrigin", shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: "none",
+        });
 
-                console.log("Made it here!")
-                console.log(ctx.session)
+        // Has the sign up for the paid subscription on installation
+        await getSubscriptionUrl(ctx, accessToken, shop);
 
-                try {
-                    const now = new Date()
-                    const values = [
-                        shop,
-                        accessToken,
-                        now,
-                        associatedUserScope,
-                        associatedUser.first_name,
-                        associatedUser.last_name,
-                        associatedUser.email,
-                        associatedUser.locale,
-                        associatedUser.account_owner,
-                        associatedUser.collaborator,
-                        associatedUser.email_verified,
-                        true
-                    ]
-                    const res = await pool.query(text, values)
-                    console.log(res.rows[0])
-                } catch (err) {
-                    console.log(err.stack)
-                }
-            },
-        }),
-    );
+        try {
+          const now = new Date();
+          const values = [
+            shop, //shop
+            accessToken, //access_token
+            now, //date_joined
+            associatedUserScope, //user_scope
+            associatedUser.first_name, //user_first_name
+            associatedUser.last_name, //user_last_name
+            associatedUser.email, //user_email
+            associatedUser.locale, //locale
+            associatedUser.account_owner, //is_owner
+            associatedUser.collaborator, //is_collaborator
+            associatedUser.email_verified, //is_email_verified
+            true, // is_active
+          ];
+          const res = await pool.query(sqlInsertUser, values);
+        } catch (err) {
+          console.log(err.stack);
+        }
+      },
+    })
+  );
 
-    server.use(graphQLProxy({version: ApiVersion.October19}))
-    server.use(verifyRequest());
-    server.use(async (ctx) => {
-        await handle(ctx.req, ctx.res);
-        ctx.respond = false;
-        ctx.res.statusCode = 200;
-        return
-    });
+  server.use(graphQLProxy({ version: ApiVersion.October19 }));
+  server.use(verifyRequest());
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return;
+  });
 
-    server.listen(port, () => {
-        console.log(`> Ready on http://localhost:${port}`);
-    });
-
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
--- server/getSubscriptionUrl.js
+++ server/getSubscriptionUrl.js
@@ -1,19 +1,11 @@
 const getSubscriptionUrl = async (ctx, accessToken, shop) => {
-    const query = JSON.stringify({
-        query: `mutation {
+  const query = JSON.stringify({
+    query: `mutation {
       appSubscriptionCreate(
           name: "Super Duper Plan"
           returnUrl: "${process.env.HOST}"
           test: true
           lineItems: [
-          {
-            plan: {
-              appUsagePricingDetails: {
-                  cappedAmount: { amount: 10, currencyCode: USD }
-                  terms: "$1 for 1000 emails"
-              }
-            }
-          }
           {
             plan: {
               appRecurringPricingDetails: {
@@ -32,21 +24,25 @@ const getSubscriptionUrl = async (ctx, accessToken, shop) => {
               id
             }
         }
-    }`
-    });
+    }`,
+  });
 
-    const response = await fetch(`https://${shop}/admin/api/2019-10/graphql.json`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            "X-Shopify-Access-Token": accessToken,
-        },
-        body: query
-    })
+  const response = await fetch(
+    `https://${shop}/admin/api/2019-10/graphql.json`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": accessToken,
+      },
+      body: query,
+    }
+  );
 
-    const responseJson = await response.json();
-    const confirmationUrl = responseJson.data.appSubscriptionCreate.confirmationUrl
-    return ctx.redirect(confirmationUrl)
+  const responseJson = await response.json();
+  const confirmationUrl =
+    responseJson.data.appSubscriptionCreate.confirmationUrl;
+  return ctx.redirect(confirmationUrl);
 };
 
 module.exports = getSubscriptionUrl;
